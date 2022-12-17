0629ca731214948b8d46b5dec15a1280e6f7da20
l4p1n <l4p1n@l4p1n.ch>
2022-12-17 19:25:43 +0100

    Add day 4 part 1

---
+++ day4/src/main.rs
@@ -0,0 +1,70 @@
+use std::fmt::{Display, Formatter};
+use std::fs;
+use std::io;
+use std::io::BufRead;
+use regex::Regex;
+
+#[derive(Copy, Clone, Debug)]
+struct AssignmentBounds {
+    pub start: u32,
+    pub end: u32,
+}
+
+impl AssignmentBounds {
+    pub fn fully_contains(&self, other: &Self) -> bool {
+        self.start >= other.start && self.end <= other.end
+    }
+}
+
+impl std::fmt::Display for AssignmentBounds {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}-{}", self.start, self.end)
+    }
+}
+
+#[derive(Copy, Clone)]
+struct AssignmentPair(AssignmentBounds, AssignmentBounds);
+
+fn main() {
+    let file_content_regex = Regex::new(r"^(\d+)-(\d+),(\d+)-(\d+)$")
+        .expect("Invalid Regex");
+
+    let filename = utils::get_file_from_argv("files/day4/test.txt");
+    let file = io::BufReader::new(fs::File::open(filename.as_ref()).unwrap());
+
+    let assignment_pairs = file.lines()
+        .map(Result::unwrap)
+        .filter_map(|line|
+            file_content_regex
+                .captures(&line)
+                .map(|captures|
+                    AssignmentPair(
+                        AssignmentBounds {
+                            start: captures.get(1).unwrap().as_str().parse().unwrap(),
+                            end: captures.get(2).unwrap().as_str().parse().unwrap(),
+                        },
+                        AssignmentBounds {
+                            start: captures.get(3).unwrap().as_str().parse().unwrap(),
+                            end: captures.get(4).unwrap().as_str().parse().unwrap(),
+                        },
+                    )
+                )
+        )
+        .collect::<Vec<_>>();
+
+    let fully_contains_count = assignment_pairs
+        .iter()
+        .inspect(
+            |pair|
+                println!(
+                    "Pair {} fully contains or is fully contained by {} ? -> {}",
+                    pair.0,
+                    pair.1,
+                    pair.0.fully_contains(&pair.1) || pair.1.fully_contains(&pair.0)
+                )
+        )
+        .filter(|pair| pair.0.fully_contains(&pair.1) || pair.1.fully_contains(&pair.0))
+        .count();
+
+    println!("Part 1: {} ranges full contains the other", fully_contains_count);
+}
